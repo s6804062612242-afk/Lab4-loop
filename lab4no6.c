6281a5453d29ecd584e43f9ec39dc4b62f035c15
6804062612242 chanasuk kongbasad <s6804062612242@email.kmutnb.ac.th>
2025-12-22 11:08:52 +0700

    display square of a character version 2

--- lab4no6.c
+++ lab4no6.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 int main(){
 	int a, b,i,j;
-	scanf("%d %d",&a,&b);
-	for(j=1;j<=5;j++){
-		for(i=1;i <= b ;i++){
-		printf("%d",a);
+	char c;
+	scanf("%c %d %d",&c,&a,&b);
+	for(i=1;i<=b;i++){
+		for(j=1;j <= a ;j++){
+		printf("%c",c);
 		}
 		printf("\n");
 	}
