c0cce40bf4425f916efef301357e2d3fbddb4c42
6804062612242 chanasuk kongbasad <s6804062612242@email.kmutnb.ac.th>
2025-12-30 14:50:21 +0700

    Add files via upload

---
+++ lab4no9.c N grade version 2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main(){
+  int score,a,j,i;
+  
+  
+  while(1==1){
+
+  scanf("%d",&score);
+  if(score==-1){
+    break;
+  }
+  if(score > 100) {
+    printf("error");
+  }
+  else if(score <0){
+    printf("error");
+  }
+  else if(score>=68){
+    if(score>=80){
+      printf("(A)\n");
+    }else if(score>=75){
+      printf("(B)\n");
+
+    }else if(score>=70){
+      printf("(C)\n");
+      a = 75 - score;
+      printf("%s:%d","to b",a);
+    }
+  }
+  else{
+    if(score>=50){
+      printf("(D)\n");
+
+    }
+    else if(score<=50){
+      printf("(F)\n");
+    }
+  }
+  }	
+  return 0;
+}
